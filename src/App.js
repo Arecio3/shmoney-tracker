3c7765d1423eb6ce950af6fd3ce1691aae7bf613
Arecio Canton <areciocodes@gmail.com>
2022-05-30 01:01:21 -0400

    Added header component and global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h2>Money Tracker &#128176;</h2>
+  )
+}
+// 128176  1F4B0
+export default Header
