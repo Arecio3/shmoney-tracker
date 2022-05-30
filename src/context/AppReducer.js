84d04efffc6166583540442d40905d44e954ada7
Arecio Canton <areciocodes@gmail.com>
2022-05-30 02:52:53 -0400

    Added delete functionailty

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,13 @@
 // Specify how app state changes in response to certain actions to Context
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                // Send back current state
+                ...state, // checks for transaction with id to delete
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
         default:
             return state;
     }
