8c1adc4020d2e3a014d1baf63852df573d3537ed
Arecio Canton <areciocodes@gmail.com>
2022-05-30 03:06:20 -0400

    You can add a new transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,4 +1,5 @@
-import React, {  useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const AddTransaction = () => {
     // Controls text for transaction
@@ -6,13 +7,27 @@ const AddTransaction = () => {
     // Controls Amount   
   const [amount, setAmount] = useState(0);
 
+  const{ addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction)
+  }
+
   return (
     <>
          <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlfor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Wtf you do with the money" />
         </div>
         <div className="form-control">
           <label htmlfor="amount"
