828b76653f2d36221af5c4458be2f032780c4ecf
Arecio Canton <areciocodes@gmail.com>
2022-05-30 02:34:24 -0400

    Added context API and dynamic style for transactions

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// Specify how app state changes in response to certain actions to Context
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
