75c396b5ed4c0a9a1997478503784413983d1cb4
Arecio Canton <areciocodes@gmail.com>
2022-05-30 02:38:44 -0400

    Showing total amount

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+  // Maps through object and grabs all amounts
+  const amounts = transactions.map(transaction => transaction.amount);
+  // Use Reduce to add them all together and toFixed to havr 2 decimal places
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </>
   )
 }
