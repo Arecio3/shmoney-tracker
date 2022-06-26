809d087c1887a3229cb97b57391fa98df10891c5
Arecio Canton <areciocodes@gmail.com>
2022-06-26 19:35:44 -0400

    You can generate a report

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,16 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import Transaction from './Transaction';
+import generatePDF from './reportGenerator';
 
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-  console.log(transactions)
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    setTickets(transactions)
+  }, [])
+
+  // console.log(transactions)
   return (
     <div>
          <h3>History</h3>
      <ul className="list">
         {transactions.map(transaction => ( <Transaction transaction={transaction} key={transaction.id}/>))}
       </ul>
+      <button onClick={() => generatePDF(transactions)}>Generate Report</button>
     </div>
   )
 }
